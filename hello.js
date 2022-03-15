c557378774e15e569200b4e315d47d436c5d987c
Kyra Henningson <kyrahenningson@hotmail.com>
2022-03-15 02:54:10 +0000

    return and console practice

--- hello.js
+++ hello.js
@@ -6,3 +6,14 @@ sayHello();
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+
+const sayHelloToConsole = function(name) {
+  console.log("Hello, "+name);
+}
+sayHelloToConsole('John');
+
+const returnSayHello = function(name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
